2fd3ed0c3f0a646dc4fba308f3393f094bc885e4
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2021-12-02 00:05:36 +0000

    name validation and basic server

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,20 @@ import styles from '../styles/Index.module.scss'
 
 export default function Index() {
 
+  function process(e) {
+    e.preventDefault();
+
+    let str = e.target.name.value.replace(/\W/g, ' ').replace(/\s\s+/g, ' ').trim();
+    console.log(str);
+  }
+
   return (
     <div className="container">
       <h1 className={styles.title}>Next Chat</h1>
+      <form className={styles.form} onSubmit={e => process(e)}>
+        <input type="text" placeholder="Enter Name" id="name"/>
+        <button type="submit">Join</button>
+      </form>
     </div>
   )
 }
